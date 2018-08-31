113afb0e1b6446aafc9cac0a9600e6d6904a3f0e
Guillaume Royer <guillaumeroyer.mail@gmail.com>
2018-08-31 11:10:59 +0100

    feat(node): add retrieve all documents

--- index.js
+++ index.js
@@ -11,17 +11,29 @@ const options = {
   accessToken: process.env.CLEARROAD_STORAGE_ACCESS_TOKEN
 };
 
-const cr = new ClearRoad(url, login, pwd, options);
+const sync = async (cr) => {
+  const now = new Date();
+  try {
+    console.log('Starting sync...');
+    await cr.sync((name) => {
+      console.log(`Finished sync of ${name}...`);
+    });
+  }
+  catch (err) {
+    console.error('An error occured:', 'message' in err ? err.message : err);
+    if (err.stack) {
+      console.error(err.stack);
+    }
+  }
+  console.log(`Done syncing in ${(new Date().getTime() - now.getTime()) / 1000}s`);
+};
 
-const now = new Date();
-console.log('Starting sync...');
+const run = async () => {
+  const cr = new ClearRoad(url, login, pwd, options);
+  await sync(cr);
 
-cr.sync()
-  .push(result => {
-    console.log('success', result);
-  }, err => {
-    console.error(err);
-  })
-  .push(() => {
-    console.log(`Done syncing in ${(new Date().getTime() - now.getTime()) / 1000}s`);
-  });
+  const documents = await cr.allDocs();
+  console.log(documents.data.rows);
+};
+
+run();
